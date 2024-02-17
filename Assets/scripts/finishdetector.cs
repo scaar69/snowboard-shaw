5a7e4f44c402daa7f378de5b2ccc2bba8b106fff
scaar69 <nivassakthi2002@gmail.com>
2024-02-17 19:03:37 +0530

    initial commit

--- Assets/scripts/finishdetector.cs
+++ Assets/scripts/finishdetector.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-
+//
 public class finishdetector : MonoBehaviour
 {
     [SerializeField] float delay = 1f;
